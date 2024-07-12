a90c085ad41cc3881381e8018410a296e85bf15e
MarcelVSHNS <54577510+MarcelVSHNS@users.noreply.github.com>
2024-07-12 18:26:28 +0200

    Refactor UNet model and update explore.py
    The UNet model in models/unet.py has been refactored to replace the "n_classes" parameter with "n_obj_preds". This change reflects the transformation of the model to predict the number of possible objects rather than specific classes. The parameters for several convolutional layers and upscaling functions were also altered accordingly. The explore.py script has been updated to use the revised UNet model and switched from torchsummary to torchinfo for model summarization.

--- explore.py
+++ explore.py
@@ -1,8 +1,8 @@
 import torch
 
 from models import UNet
-from torchsummary import summary
+from torchinfo import summary
 
 model = UNet(n_channels=3,
-             n_classes=2)
-summary(model, input_size=(3, 1280, 1920), device=torch.device('cpu'))
+             n_obj_preds=16)
+summary(model, input_size=(1, 3, 1280, 1920), device=torch.device('cpu'))
--- models/unet.py
+++ models/unet.py
@@ -6,23 +6,26 @@ import torch.nn.functional as F
 
 
 class UNet(nn.Module):
-    def __init__(self, n_channels, n_classes, bilinear=False):
+    def __init__(self, n_channels, n_obj_preds):
+        """
+        Initializes the UNet class.
+        :param n_channels: Number of input channels.
+        :param n_obj_preds: Number of object occurrences. Means how many possible objects per row
+        """
         super(UNet, self).__init__()
         self.n_channels = n_channels
-        self.n_classes = n_classes
-        self.bilinear = bilinear
+        self.n_classes = n_obj_preds
 
         self.inc = (DoubleConv(n_channels, 64))
-        self.down1 = (Down(64, 128))
-        self.down2 = (Down(128, 256))
-        self.down3 = (Down(256, 512))
-        factor = 2 if bilinear else 1
-        self.down4 = (Down(512, 1024 // factor))
-        self.up1 = (Up(1024, 512 // factor, bilinear))
-        self.up2 = (Up(512, 256 // factor, bilinear))
-        self.up3 = (Up(256, 128 // factor, bilinear))
-        self.up4 = (Up(128, 64, bilinear))
-        self.outc = (OutConv(64, n_classes))
+        self.down1 = (Down(64, 128, 1))
+        self.down2 = (Down(128, 256, 2))
+        self.down3 = (Down(256, 512, 3))
+        self.down4 = (Down(512, 512, 4))
+        self.up1 = (Up(1024, 256, 1))
+        self.up2 = (Up(512, 128, 2))
+        self.up3 = (Up(256, 64, 3))
+        self.up4 = (Up(128, 64, 4))
+        self.outc = (OutConv(64, n_obj_preds))
 
     def forward(self, x):
         x1 = self.inc(x)
@@ -34,8 +37,8 @@ class UNet(nn.Module):
         x = self.up2(x, x3)
         x = self.up3(x, x2)
         x = self.up4(x, x1)
-        logits = self.outc(x)
-        return logits
+        pred = self.outc(x)
+        return pred
 
     def use_checkpointing(self):
         self.inc = checkpoint(self.inc)
@@ -71,7 +74,7 @@ class DoubleConv(nn.Module):
 
 class Down(nn.Module):
     """Downscaling with maxpool then double conv"""
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels, out_channels, stage):
         super().__init__()
         self.maxpool_conv = nn.Sequential(
             nn.MaxPool2d(2),
@@ -84,26 +87,22 @@ class Down(nn.Module):
 
 class Up(nn.Module):
     """Upscaling then double conv"""
-    def __init__(self, in_channels, out_channels, bilinear=True):
+    def __init__(self, in_channels, out_channels, stage):
         super().__init__()
-        # if bilinear, use the normal convolutions to reduce the number of channels
-        if bilinear:
-            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-            self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
-        else:
-            self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
-            self.conv = DoubleConv(in_channels, out_channels)
+        self.up = nn.Upsample(scale_factor=(1, 1.193), mode='bilinear', align_corners=True)
+        self.long = nn.MaxPool2d(kernel_size=(2, 1), stride=(2, 1))
+        self.skip = nn.MaxPool2d(kernel_size=(stage ** 2, stage), stride=(stage ** 2, stage))
+        self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
 
     def forward(self, x1, x2):
         x1 = self.up(x1)
-        # input is CHW
-        diffY = x2.size()[2] - x1.size()[2]
-        diffX = x2.size()[3] - x1.size()[3]
-        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
-                        diffY // 2, diffY - diffY // 2])
-        # if you have padding issues, see
-        # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
-        # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
+        x1 = self.long(x1)
+        x2 = self.skip(x2)
+        x1_height, x1_width = x1.size()[2:]
+        x2_height, x2_width = x2.size()[2:]
+        diffY = x2_height - x1_height
+        diffX = x2_width - x1_width
+        x2 = x2[:, :, diffY // 2: diffY // 2 + x1_height, diffX // 2: diffX // 2 + x1_width]
         x = torch.cat([x2, x1], dim=1)
         return self.conv(x)
 
@@ -111,7 +110,8 @@ class Up(nn.Module):
 class OutConv(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(OutConv, self).__init__()
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=(4, 1), stride=(1, 1), padding=(1, 0))
+        # self.conv = F.interpolate(input_tensor, size=(4, 240), mode='linear', align_corners=False)
 
     def forward(self, x):
         return self.conv(x)
