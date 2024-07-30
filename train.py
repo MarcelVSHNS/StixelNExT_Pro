efb078ff402b58452375b8275c7f445f8e979db8
MarcelVSHNS <54577510+MarcelVSHNS@users.noreply.github.com>
2024-07-30 09:40:42 +0200

    added loading msg

--- train.py
+++ train.py
@@ -91,6 +91,8 @@ def train(rank, world_size):
         if rank == 0 and os.path.isfile(config['load_checkpoint']):
             start_epoch, loss = load_checkpoint(model, optimizer, config['load_checkpoint'])
             print(f"Checkpoint {os.path.basename(config['load_checkpoint'])} loaded. Continue training on epoch {start_epoch} with loss {loss}.")
+        else:
+            print(f"Check file path: {config['load_checkpoint']}. No Checkpoint is loaded!")
 
     # Initialize Logger
     if config['logging'] and rank == 0:
