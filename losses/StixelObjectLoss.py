da7d3f6a0e32ceb98d58ed52cd2bf39a082c14a4
MarcelVSHNS <54577510+MarcelVSHNS@users.noreply.github.com>
2024-07-31 16:15:42 +0200

    minor bug fixes and change of upsampling method to bilinear

--- losses/StixelObjectLoss.py
+++ losses/StixelObjectLoss.py
@@ -24,7 +24,7 @@ class StixelObjectLoss(nn.Module):
             self.weights = weights
         # Focal Loss focus more on hard samples. BCE: universal probability loss
         # self.classify_loss = focal_loss.sigmoid_focal_loss
-        self.class_loss: nn.BCELoss = nn.BCELoss()
+        self.classify_loss: nn.BCELoss = nn.BCELoss(reduction="mean")
         # MSE: bottom point position loss + stixel/ object length loss, ...
         self.regress_loss: nn.MSELoss = nn.MSELoss(reduction="none")
         # self.regress_loss: nn.SmoothL1Loss = nn.SmoothL1Loss()
@@ -56,7 +56,7 @@ class StixelObjectLoss(nn.Module):
         mask = (targets[:, p_idx, :, :] > 0).float()
 
         # currently no matching is implemented, double loss as possible strategy
-        depth_bin_loss = self.classify_loss(inputs[:, p_idx, :, :], targets[:, p_idx, :, :], alpha=0.25, gamma=2, reduction="mean") * self.weights['P']
+        depth_bin_loss = self.classify_loss(inputs[:, p_idx, :, :], targets[:, p_idx, :, :]) * self.weights['P']
         bottom_loss = self.regress_loss(inputs[:, vb_idx, :, :], targets[:, vb_idx, :, :]) * self.weights['vB']
         top_loss = self.regress_loss(inputs[:, vt_idx, :, :], targets[:, vt_idx, :, :]) * self.weights['vT']
         # depth_loss = self.regress_loss(inputs[:, 2, :, :], targets[:, 2, :, :]) * self.weights['d']
