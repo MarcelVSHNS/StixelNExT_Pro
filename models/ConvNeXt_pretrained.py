1aae03f724154cc9f770c68e6f538bb8c8b0d331
Marcel <54577510+MarcelVSHNS@users.noreply.github.com>
2024-12-15 12:59:09 +0100

    adjusted wrong function

--- models/ConvNeXt_pretrained.py
+++ models/ConvNeXt_pretrained.py
@@ -74,7 +74,7 @@ class StixelHead(nn.Module):
         self.up = nn.Sequential(
             nn.Upsample(size=(1, 240), mode='nearest'),
             norm_layer(out_channels))
-        self.channel_reduce = nn.Conv2d(in_channels=in_channels * 2, out_channels=out_channels, kernel_size=1)
+        self.channel_reduce = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=1)
         # self.attention_layer = ColumnAttention(768)
         # self.attention_influence = partial(combine_attention_prediction, method="concat")
         self.out_channels = out_channels
