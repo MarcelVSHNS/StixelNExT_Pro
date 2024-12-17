22266bba4c46266c64629deaf5165b9e458e1a2d
MarcelVHsE <marcel.vosshans@hs-esslingen.de>
2024-12-17 11:05:41 +0100

    changed distributed port number to enable parallel training with origin stixelnext

--- train.py
+++ train.py
@@ -34,7 +34,7 @@ overall_start_time = datetime.now()
 
 def setup(rank, world_size):
     os.environ['MASTER_ADDR'] = 'localhost'
-    os.environ['MASTER_PORT'] = '12355'
+    os.environ['MASTER_PORT'] = '12356'
     # 'gloo' for CPUs, 'nccl' for GPUs
     dist.init_process_group("nccl", rank=rank, world_size=world_size)
     torch.cuda.set_device(rank)
